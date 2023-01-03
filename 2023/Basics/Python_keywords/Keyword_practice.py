085a99a13c48f3d77ae224e23dad857e9431bc13
visweswar1999 <visweswar1999>
2023-01-03 21:15:22 +0530

    03012022

---
+++ 2023/Basics/Python_keywords/Keyword_practice.py
@@ -0,0 +1,9 @@
+#to get what are the keywords are available
+
+import keyword
+
+a = keyword.kwlist
+
+print(a)
+
+print(help('False'))
---
+++ 2023/Basics/operators/operators_practice.py
@@ -0,0 +1,19 @@
+
+'''
+#multiplication
+
+
+a = 10
+b = 20
+
+print(a*b)
+#division opertaion
+
+b = 20
+a = 10
+
+print(b/a)'''
+
+a = 20
+b = 4
+print(a%b)
